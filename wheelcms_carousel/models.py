992db59ea5a191a5551d6280266e0a045f8b5451
Ivo van der Wijk <github@in.m3r.nl>
2013-05-22 15:58:00 +0200

    basic project code (taken from wheelsite_site)

---
+++ wheelcms_carousel/models.py
@@ -0,0 +1,24 @@
+from wheelcms_axle.templates import template_registry
+from wheelcms_spokes.page import Page, PageType
+from wheelcms_axle.content import ImageContent
+
+def frontpage_context(handler, request, node):
+    ## limit to images, visible
+    datanode = node.child('data')
+    if datanode is None:
+        return dict(carousel=[])
+
+    return dict(carousel=[n for n in node.child('data').children()
+                          if isinstance(n.content(), ImageContent)
+                          and n.content().spoke().workflow().is_visible()
+                         ],
+                slots=[s for s in node.child('data').children()
+                          if isinstance(s.content(), Page)
+                          and s.content().spoke().workflow().is_visible()
+                         ],
+               )
+
+
+template_registry.register(PageType, "wheelsite_site/page_frontpage_view.html",
+                           "Frontpage view", context=frontpage_context)
+
